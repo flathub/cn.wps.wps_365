069451cfc94371d863a9a7753c2b45e3e982cff5
Anlor <chenxiex@outlook.com>
2025-12-18 12:36:02 +0800

    Fix backup path config

--- wps_runner.sh
+++ wps_runner.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 DEFAULT_DATA_HOME="${HOME}/.local/share"
 DISABLE_FORCE_LOGIN_DELAY="${DISABLE_FORCE_LOGIN_DELAY:-3}"
+CONF_FILE="${XDG_CONFIG_HOME}/Kingsoft/Office.conf"
+BACKUPS_SUBDIR="Kingsoft/office6/data/backup"
+OLD_BACKUP_PATH="${DEFAULT_DATA_HOME}/${BACKUPS_SUBDIR}"
+NEW_BACKUP_PATH="${XDG_DATA_HOME}/${BACKUPS_SUBDIR}"
+
+# Set backup path to correct dir under XDG_DATA_HOME in the config
+if [ ! -f "${CONF_FILE}" ]; then
+    msg "No config exists, creating one"
+    mkdir -p "${NEW_BACKUP_PATH}"
+    mkdir -p "$(dirname "${CONF_FILE}")"
+    cat <<EOF > "${CONF_FILE}"
+[6.0]
+common\Backup\AutoRecoverFilePath=${NEW_BACKUP_PATH}
+wpsoffice\Application%20Settings\AppComponentMode=prome_independ
+EOF
+elif grep -q "${OLD_BACKUP_PATH}" "${CONF_FILE}"; then
+    msg "Config file contains old paths, updating"
+    mkdir -p "${NEW_BACKUP_PATH}"
+    sed "s|${OLD_BACKUP_PATH}|${NEW_BACKUP_PATH}|g" -i "${CONF_FILE}"
+fi
 
 # Symlink hardcoded datadir to XDG_DATA_HOME
 if [ ! -d "${DEFAULT_DATA_HOME}/Kingsoft" ]; then
